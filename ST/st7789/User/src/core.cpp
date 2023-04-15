ef084dff068920270f6f672e4bc9577f5e4c9406
TheBadZhang <thebadzhang@qq.com>
2023-04-15 16:45:52 +0800

    看不透，看不透，这写的是什么东西呢。。。

---
+++ ST/st7789/User/src/core.cpp
@@ -0,0 +1,9 @@
+
+#include "core.h"
+
+extern SPI_HandleTypeDef hspi1;
+
+sdfsdfslkdjfskldfjklsdsldkfjskdjfksjdkfjsdkjfksdjfkjs
+
+你他妈就是一个傻逼的吧
+z
---
+++ ST/st7789/src/sys.h
@@ -0,0 +1,12 @@
+#ifndef __TBZ_SYS_H_
+#define __TBZ_SYS_H_
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint8_t uchar;
+typedef uint32_t uint;
+void delay_us(uint32_t time);
+
+
+#endif
